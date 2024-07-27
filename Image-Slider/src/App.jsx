134d5f67f11389962d505020520f9a86f4aebdd4
Piyush Channawar <channawarpiyush@gmail.com>
2024-07-27 14:47:17 +0530

    Created new project Image slider

---
+++ Image-Slider/src/App.jsx
@@ -0,0 +1,17 @@
+import ImageSlider from "./Components/ImageSlider"
+
+function App() {
+  
+
+  return (
+   <div>
+      <ImageSlider 
+      url={"https://picsum.photos/v2/list"}
+      page={"1"}
+      limit={"10"}
+      />
+   </div>
+  )
+}
+
+export default App
